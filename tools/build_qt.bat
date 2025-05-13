6790fe0962f6b43a4e3bf6ef4b597a2e65498d8a
ComplexRobot <complexrobotx@gmail.com>
2025-05-13 04:17:10 -0700

    Update workflow. Update to correct cpu core count.

--- tools/build_qt.bat
+++ tools/build_qt.bat
@@ -4,13 +4,13 @@ setlocal
 cd %QTBUILDDIR% ||  exit /b 1
 
 echo Building Qt...
-call jom -j16
+call jom -j%NUMBER_OF_PROCESSORS%
 IF %errorlevel% NEQ 0 (
     exit /b 1
 )
 
 echo Installing Qt...
-call jom -j16 install
+call jom -j%NUMBER_OF_PROCESSORS% install
 IF %errorlevel% NEQ 0 (
     exit /b 1
 )
